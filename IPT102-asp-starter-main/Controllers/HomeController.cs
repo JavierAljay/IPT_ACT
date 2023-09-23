7a627a305b298912e382ee3f2a1d40df9f8e14e7
Aljay Javier <javier.aljay.06232003@gmail.com>
2023-09-23 12:30:09 +0800

    Names Updated

---
+++ IPT102-asp-starter-main/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        var studentInfoArray = new[]
+        {
+            new StudentInfoModel { Name = "Aljay Javier", StudentId = "21-1480", StudentAge = "20" },
+            new StudentInfoModel { Name = "Jinalyn Javier", StudentId = "21-1481", StudentAge = "37" },
+            new StudentInfoModel { Name = "Aldwin Javier", StudentId = "21-1482", StudentAge = "39" }
+        };
+
+        return View(studentInfoArray);
+    }
+}
